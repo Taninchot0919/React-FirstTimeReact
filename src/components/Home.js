01fa28ef8831fedaa5796526aa87b1aa56fcf322
Taninchot Art <Taninchot.art@mail.kmutt.ac.th>
2021-05-31 00:16:55 +0700

    [UPDATE] Make a blog list and adding css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,30 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("mario");
-  const [age, setAge] = useState(20);
-  const handleClick = () => {
-    setName("luigi");
-    setAge(35);
-  };
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My Title 1",
+      body: "lorem ipsum ...",
+      author: "Taninchot",
+      id: 1,
+    },
+    { title: "My Title 2", body: "lorem ipsum ...", author: "Art", id: 2 },
+    {
+      title: "My Title 3",
+      body: "lorem ipsum ...",
+      author: "Phuwaloertthiwat",
+      id: 3,
+    },
+  ]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} years old
-      </p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written By : {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
