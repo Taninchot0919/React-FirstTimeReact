eea8bc1b3ddc22d2756c75f718e9bd55bde194a2
Taninchot Art <Taninchot.art@mail.kmutt.ac.th>
2021-05-30 23:54:30 +0700

    [UPDATE] On click with parameter

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+const Home = () => {
+  const handleClick = () => {
+    console.log("You clicked");
+  };
+
+  const handleClickAgain = (name) => {
+    console.log("Hello, " + name);
+  };
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={ ()=>{handleClickAgain('Art') }}>Click me again</button>
+    </div>
+  );
+};
+
+export default Home;
