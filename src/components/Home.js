0e385ca7c110aebd04d35871d2b5fe5eda12b7e7
Taninchot Art <Taninchot.art@mail.kmutt.ac.th>
2021-05-31 01:24:12 +0700

    [UPDATE] Learn fetching data in react

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,43 +2,22 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlockList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "My Title 1",
-      body: "lorem ipsum ...",
-      author: "Taninchot",
-      id: 1,
-    },
-    {
-      title: "My Title 2",
-      body: "lorem ipsum ...",
-      author: "Taninchot",
-      id: 2,
-    },
-    {
-      title: "My Title 3",
-      body: "lorem ipsum ...",
-      author: "Phuwaloertthiwat",
-      id: 3,
-    },
-  ]);
-
-  const [name, setName] = useState("Taninchot");
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id); // ถ้าต้องการให้ลบออกต้องทำให้เป็น false
-    setBlogs(newBlogs); // เอามาจากตอน useState
-  };
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    // จะทำงานเมื่อเกิดการ Re-Render
-    console.log("useEffect ran");
-    console.log(name);
+    fetch("http://localhost:9000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
   }, []); // การใส่ Array เนี่ยเหมือนเป็นการตั้งว่าจะใช้ useEffect กับแค่ใน Array นี้และเริ่มต้น
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} />
+      {blogs && <BlogList blogs={blogs} title="All blogs!" />}
     </div>
   );
 };
