88df4021c1fd1bd359da69799b3ead24b04e8c0e
Taninchot Art <Taninchot.art@mail.kmutt.ac.th>
2021-05-31 13:04:02 +0700

    [UPDATE] Learn to reuse custom hook

--- src/components/BlockDetails.js
+++ src/components/BlockDetails.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { useParams } from "react-router";
+import useFetch from "./UseFetch";
 const BlogDetails = () => {
   const { id } = useParams();
+  const {
+    data: blog,
+    error,
+    isPending,
+  } = useFetch("http://localhost:9000/blogs/" + id);
 
   return (
     <div className="blog-details">
-      <h2>Blog Details : {id}</h2>
+      {isPending && <div>Loading ...</div>}{" "}
+      {/*จะทำงานก็ต่อเมื่อด้านซ้ายเป็นจริง */}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by : {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
